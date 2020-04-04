b3dcccab42cf2b4f2716c44e1fe41783283700cc
elenaslavutina <elena.slavutina2013@gmail.com>
2020-04-04 09:42:10 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    long result = service.calculate(amount, registered);
+    System.out.println(result);
 
-    service.calculate(amount, registered);
   }
 }
 
